73d743cb6b3e982646df4c7c580da96a744b4166
Wan Nashrul Haqeem <haqeemwan.official@gmail.com>
2023-11-24 12:43:13 +0800

    Initial React Commit - 241123

---
+++ project-file/src/components/ListGroup.tsx
@@ -0,0 +1,33 @@
+import { MouseEvent } from "react";
+
+function ListGroup() {
+  const items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
+
+  /*
+  const getMessage = () => {
+    return items.length === 0 ? <p>No Item Foundd</p> : null;
+  };
+*/
+
+const handleClick = (event: MouseEvent) => console.log(event);
+
+  return (
+    <>
+      <h1>Test Document</h1>
+      {items.length === 0 && <p>No Item Foundd</p>}
+      <ul className="list-group">
+        {items.map((item, index) => (
+          <li
+            className="list-group-item"
+            key={item}
+            onClick={handleClick}
+          >
+            {item}
+          </li>
+        ))}
+      </ul>
+    </>
+  );
+}
+
+export default ListGroup;
