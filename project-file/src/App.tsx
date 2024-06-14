11d4b172ba63e62210a9d843dce7c7d03e009b9a
Wan Nashrul Haqeem <haqeemwan.official@gmail.com>
2024-06-14 20:35:10 +0800

    Final Update Before Website Renovation - 14/06/2024

--- project-file/src/App.tsx
+++ project-file/src/App.tsx
@@ -1,10 +1,19 @@
+import React, { useEffect } from "react";
 import "./App.css";
-
-import ListGroup from "./components/ListGroup";
 import ParallaxFrame from "./components/ParallaxFrame";
 import IntroVideo from "./components/IntroVideo";
 
 function App() {
+  useEffect(() => {
+    if ("scrollRestoration" in window.history) {
+      window.history.scrollRestoration = "manual";
+    }
+    console.log("Scrolling to top");
+    setTimeout(() => {
+      window.scrollTo(0, 0);
+    }, 0);
+  }, []);
+
   return (
     <div className="background-colour-website">
       <ParallaxFrame />
